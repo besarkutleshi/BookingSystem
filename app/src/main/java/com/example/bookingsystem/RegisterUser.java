a1d15a92ded255bc4326df4fef0010f48c6b693a
Besar Kutleshi <besar.kutleshi0@gmail.com>
2020-04-15 01:06:16 +0200

    Bottom navigation menu created

--- app/src/main/java/com/example/bookingsystem/RegisterUser.java
+++ app/src/main/java/com/example/bookingsystem/RegisterUser.java
@@ -3,9 +3,11 @@ package com.example.bookingsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegisterUser extends AppCompatActivity {
-
+    Button btnRegister;
+    Button btnClose;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
