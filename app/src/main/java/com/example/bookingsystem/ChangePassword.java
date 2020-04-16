be47f3a17bc0a116fc9eddc89994e41d359c9573
Besar Kutleshi <besar.kutleshi0@gmail.com>
2020-04-17 01:27:33 +0200

    Change password activity created

--- app/src/main/java/com/example/bookingsystem/ChangePassword.java
+++ app/src/main/java/com/example/bookingsystem/ChangePassword.java
@@ -1,14 +1,40 @@
 package com.example.bookingsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ChangePassword extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigation;
+    Intent favorites;
+    Intent home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+        favorites = new Intent(this, Favorites.class);
+        home = new Intent(this, Booking.class);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(nav);
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener nav = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.navigation_favorites:
+                    startActivity(favorites);
+                    return  true;
+                case R.id.navigation_home:
+                    startActivity(home);
+                    return true;
+            }
+            return false;
+        }
+    };
 }
